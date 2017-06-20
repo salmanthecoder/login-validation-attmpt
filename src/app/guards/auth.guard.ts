07b5694ed44bfae4a0e64d1c101abbc9b698f3ef
salmanthecoder <rahilsalman@gmail.com>
2017-06-20 22:01:57 +0530

    services for notifications and getting details

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,16 @@
+﻿import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+    constructor(private router: Router) { }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        if (localStorage.getItem('loggedinUser')) {
+            // logged in so return true
+            return true;
+        }
+        return false;
+    }
+}
