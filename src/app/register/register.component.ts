2db12469a1a8e9ed6a84b8fab7d896ec5e8b10eb
salmanthecoder <rahilsalman@gmail.com>
2017-06-20 22:09:45 +0530

    corrected models and register component

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,32 @@
+﻿//register
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AlertService, UserService } from '../services/index';
+
+@Component({
+    moduleId: module.id.toString(),
+    templateUrl: 'register.component.html'
+})
+
+export class RegisterComponent {
+    model: any = {};
+    loading = false;
+
+    constructor(
+        private router: Router,
+        private userService: UserService,
+        private alertService: AlertService) { }
+
+    register() {
+        this.loading = true;
+        this.userService.create(this.model)
+            .subscribe(
+                data => {
+                    this.alertService.success('you have successfully registered', true);
+                },
+                error => {
+                    this.loading = false;
+                });
+    }
+}
