393527d445c4a3fbd4b4b50294cdef2eba809c31
salmanthecoder <rahilsalman@gmail.com>
2017-06-20 21:45:41 +0530

    configured routing and app component creation

---
+++ src/app/app.routing.ts
@@ -0,0 +1,14 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+
+
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
