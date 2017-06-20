18212d84d67d61d558d3d1e2092053d87f34f88b
salmanthecoder <rahilsalman@gmail.com>
2017-06-20 23:21:04 +0530

    added component to main app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { fakeBackendProvider } from './helpers/index';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { BaseRequestOptions } from '@angular/http';
+
 import { AppComponent }  from './app.component';
 import { routing }        from './app.routing';
 
+import { AlertComponent } from './directives/index';
+import { AuthGuard } from './guards/index';
+import { AlertService, AuthenticationService, UserService } from './services/index';
+import { HomeComponent } from './home/index';
+import { LoginComponent } from './login/index';
+import { RegisterComponent } from './register/index';
 
 @NgModule({
     imports: [
@@ -15,9 +25,19 @@ import { routing }        from './app.routing';
     ],
     declarations: [
         AppComponent,
+        AlertComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent
     ],
     providers: [
-        
+        AuthGuard,
+        AlertService,
+        AuthenticationService,
+        UserService,
+        fakeBackendProvider,
+        MockBackend,
+        BaseRequestOptions
     ],
     bootstrap: [AppComponent]
 })
